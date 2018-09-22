6fd14572a974231b0c42abf0043f39822f9332be
Inma Ordóñez <iordonezmarvizon@gmail.com>
2018-09-22 08:45:38 +0200

    create nav component

--- src/components/header/header-component.js
+++ src/components/header/header-component.js
@@ -1,8 +1,11 @@
 import './header-component.scss';
+import { makeNav } from '../nav/nav-component';
 
 export const makeHeader = ({ title }) => {
     const header = document.createElement('header');
+    header.id = 'top';
     header.innerHTML = `<h1 class="title">${title}</h1>`;
+    header.appendChild(makeNav());
     return header;
 }
 
---
+++ src/components/nav/nav-component.js
@@ -0,0 +1,9 @@
+import './nav-component.scss';
+
+export const makeNav = () => {
+    const nav = document.createElement('nav');
+    nav.innerHTML = `<div class="tags-wrapper">Tags go here</div>`;
+    return nav;
+}
+
+export default { makeNav };
