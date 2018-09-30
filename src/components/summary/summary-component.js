edf898ad4c8f2b230071c6143b25d9ee98ddfd66
Inma Ordóñez <iordonezmarvizon@gmail.com>
2018-09-30 10:17:30 +0200

    summaries styling

--- src/components/summary/summary-component.js
+++ src/components/summary/summary-component.js
@@ -3,9 +3,9 @@ import './summary-component.scss';
 export const makeSummary = ({ title, author, brief, picUrl, commentCount }) => { // eslint-disable-line
   const summary = document.createElement('div');
   summary.classList.add('summary');
-  summary.innerHTML = `<h2 class="summary-title">${title}</h2>
+  summary.innerHTML = `<img src="${picUrl}">
   <div class="details">
-    <img src="${picUrl}">
+    <h2 class="summary-title">${title}</h2>
     <div class="brief">${brief}</div>
     <div class="author">By: ${author}</div>
     <div class="comment-count">
